98198d90a4ad6f710a87c639939cb987d4686d7a
William Rodgers <william.rodgers@stu.ocsb.ca>
2024-10-07 11:12:54 -0400

    Oh yeah forgot this whoops

--- main.js
+++ main.js
@@ -18,15 +18,15 @@ let length = 10
 let width = 5
 let area = length * width
 
-let first_name = "Santa "
+let first_name = "Santa"
 let last_name = "Claws"
 last_name = "Claus"
 
 const pi = 3.141592653589793438462643383279
 let r = 5
-let circle_area = pi * r
+let circle_area = pi * 2 * r
 
 console.log("Area: " + area)
-console.log("I still believe in " + first_name + last_name)
+console.log("I still believe in " + first_name + " " + last_name)
 console.log(typeof area, last_name)
 console.log(circle_area)
