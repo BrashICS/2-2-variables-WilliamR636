f68ce2859c6371865b6aa2a82f85ae6c675032bd
William Rodgers <william.rodgers@stu.ocsb.ca>
2024-10-07 10:57:01 -0400

    Here you go

--- main.js
+++ main.js
@@ -13,3 +13,20 @@
 let it_is_christmas = false
 let teaching_partner = "Mr. Squirrel"
 let number_of_students = 27
+
+let length = 10
+let width = 5
+let area = length * width
+
+let first_name = "Santa "
+let last_name = "Claws"
+last_name = "Claus"
+
+const pi = 3.141592653589793438462643383279
+let r = 5
+let circle_area = pi * r
+
+console.log("Area: " + area)
+console.log("I still believe in " + first_name + last_name)
+console.log(typeof area, last_name)
+console.log(circle_area)
